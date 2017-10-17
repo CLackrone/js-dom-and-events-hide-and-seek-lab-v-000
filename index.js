6fd9be8835d2387f82765aedd5712b1cf7b94b61
CLackrone <charisaanne@gmail.com>
2017-10-17 14:27:12 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+//returns the first match
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+  let grandNode = document.getElementById('grand-node')
+  let childNode = grandNode.children[0]
+
+  while (childNode) {
+    grandNode = childNode
+    childNode = grandNode.children[0]
+  }
+
+  return grandNode
+}
+
+function increaseRankBy(n) {
+  let list = document.getElementsByClassName('ranked-list')
+
+  for (let index in list) {
+    list[index].innerHTML = parseInt(list[index]) + n
+  }
+}
+
